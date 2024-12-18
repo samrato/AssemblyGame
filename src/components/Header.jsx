d647c529c303c8613efda47ec16007b19b1bb7ac
Willington Juma <146538882+samrato@users.noreply.github.com>
2024-12-19 00:46:30 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <div className='text-center'>
-      <h1 className='font-semibold'>Assembly :End Game </h1>
-      <p className='font-light '>Gause The word within 8 attemps to keep the 
+      <h1 className='font-semibold'>Assembly :End Game. </h1>
+      <p className='font-light '>Gause The word within 8 attemps to keep the  going and
       programming world safe from Assembly!</p>
     </div>
   )
